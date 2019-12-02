b9df8dfc15312ae96eb9b94d9095e49bc6888503
mluquemeijide <54317318+mluquemeijide@users.noreply.github.com>
2019-12-02 12:58:50 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Watson-Restaurant-Chatbot
+# Watson restaurant chatbot
+
+# Intructions
+
+Create the `.env` file with the same content of `.env.example` and add your keys.
+
+Example of how `.env` will look:
+```
+APIKEY=<apikey>
+WORKSPACE_ID=<workspace_id>
+```
+
+To run the project execute in your terminal `node app.js`
---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('./public'));
+
+const port = 3000;
+
+const assistant = new AssistantV1({
+  iam_apikey: process.env.APIKEY,
+  url: 'https://gateway.watsonplatform.net/assistant/api/',
+  version: '2018-02-16',
+});
+
+app.post('/conversation/', (req, res) => {
+  const { text, context = {} } = req.body;
+
+  const params = {
+    input: { text },
+    workspace_id: process.env.WORKSPACE_ID,
+    context,
+  };
+
+  assistant.message(params, (err, response) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json(err);
+    } else {
+      res.json(response);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
